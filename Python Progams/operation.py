28ad08aa6c9148a0ae988f382d9caec9129130ea
akash <akash300383@gmail.com>
2022-09-06 12:53:28 +0530

    done:

---
+++ Python Progams/operation.py
@@ -0,0 +1,12 @@
+a=10
+b=3
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+print("sum of two number is ",sum)
+print("sub of two number is ",sub)
+print("mul of two number is ",mul)
+print("div of two number is ",div)
+print("mod of two number is ",mod)
---
+++ Python Progams/profile.py
@@ -0,0 +1,8 @@
+roll_number="Ab12Xy"
+fname="Raj"
+lname="Deep"
+print("======================");
+print("My Profile detais is ")
+print("My Roll Number is ",roll_number);
+print("My first name is ",fname,"\nLast name is ",lname);
+print("=======================")
